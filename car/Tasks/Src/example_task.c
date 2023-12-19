095728eae0bc1deff879da4cec7f11bc0ca1db56
zbf <2265011424@qq.com>
2023-12-19 19:50:33 +0800

    trace module!

--- car/Tasks/Src/example_task.c
+++ car/Tasks/Src/example_task.c
@@ -49,131 +49,110 @@ void Example_task(void * arg) {
 	/* USER CODE END */
 	while(1) {
 		/*
-		if (control.channel[1] > 0.6 && control.channel[0] >= (-0.2) && control.channel[0] <= 0.2){
-			HAL_GPIO_WritePin(GPIOB,GPIO_PIN_8,1);
-			HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,0);
-			PWM_SetDutyRatio(&pwm2[3], control.channel[1]);
-			PWM_SetDutyRatio(&pwm2[4], control.channel[1]);
-			HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,1);
-			HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,0);
-			PWM_SetDutyRatio(&pwm2[1], control.channel[1]);
-			PWM_SetDutyRatio(&pwm2[2], control.channel[1]);
-		}
-		else if (control.channel[1] < (-0.6) && control.channel[0] >= (-0.2) && control.channel[0] <= 0.2){
-			HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,0);  //IN1
-			HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,1);  //IN2
-			float newforward = -1*forward;
-			PWM_SetDutyRatio(&pwm2[1], -1*control.channel[1]);  //LEFT BACK
-			PWM_SetDutyRatio(&pwm2[2], -1*control.channel[1]);  //RIGHT BACK
-			HAL_GPIO_WritePin(GPIOB,GPIO_PIN_8,0);  //IN3
-			HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,1);	//IN4
-			PWM_SetDutyRatio(&pwm2[3], -1*control.channel[1]);  //LEFT FRONT
-			PWM_SetDutyRatio(&pwm2[4], -1*control.channel[1]);  //RIGHT FRONT
-		}
-		else if (control.channel[1] > (-0.2) && control.channel[1] < 0.2){
-			if (control.channel[0] > 0.6){
-				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,1);  //IN1
-				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,0);  //IN2
-				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_8,0);  //IN3
-				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,0);	//IN4
-				PWM_SetDutyRatio(&pwm2[1],control.channel[0]);
-				PWM_SetDutyRatio(&pwm2[2],control.channel[0]);
-				PWM_SetDutyRatio(&pwm2[3],control.channel[0]);
-				PWM_SetDutyRatio(&pwm2[4],control.channel[0]);
-				delay(5);
-			}
-			else if (control.channel[0] < (-0.6)){
-				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,0);  //IN1
-				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,0);  //IN2
-				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_8,1);  //IN3
-				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,0);	//IN4
-				float newdirection = direction;
-				PWM_SetDutyRatio(&pwm2[1],-1*control.channel[0]);
-				PWM_SetDutyRatio(&pwm2[2],-1*control.channel[0]);
-				PWM_SetDutyRatio(&pwm2[3],-1*control.channel[0]);
-				PWM_SetDutyRatio(&pwm2[4],-1*control.channel[0]);
-			}
-			else if (control.channel[0] >=(-0.2) && control.channel[0]<=0.2){
-				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,0);  //IN1
-				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,0);  //IN2
-				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_8,0);  //IN3
-				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,0);	//IN4
-				PWM_SetDutyRatio(&pwm2[1],control.channel[0]);
-				PWM_SetDutyRatio(&pwm2[2],control.channel[0]);
-				PWM_SetDutyRatio(&pwm2[3],control.channel[0]);
-				PWM_SetDutyRatio(&pwm2[4],control.channel[0]);
-			}
-			*/
-			
-			
-			
-			
+		!!!!!this is controller!!!!! Don't use trace module at a same time!!!!!!!!!!!!!!!
 			if (control.channel[1] > 0.2){
-				//front
-				PWM_SetDutyRatio(&pwm2[1],1);
-				PWM_SetDutyRatio(&pwm2[2],0);
-				PWM_SetDutyRatio(&pwm2[3],1);
-				PWM_SetDutyRatio(&pwm2[4],0);
-				//back
 				PWM_SetDutyRatio(&pwm1[4],1);
 				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,0);
 				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,1);
 				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,0);
+				PWM_SetDutyRatio(&pwm2[1],1);
+				PWM_SetDutyRatio(&pwm2[2],1);
 			}
 			if (control.channel[1] < -0.2){
-				//
-				PWM_SetDutyRatio(&pwm2[1],0);
-				PWM_SetDutyRatio(&pwm2[2],1);
-				PWM_SetDutyRatio(&pwm2[3],0);
-				PWM_SetDutyRatio(&pwm2[4],1);
-				//back
 				PWM_SetDutyRatio(&pwm1[4],0);
 				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,1);
 				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,0);
 				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,1);
+				PWM_SetDutyRatio(&pwm2[1],1);
+				PWM_SetDutyRatio(&pwm2[2],1);
+				
 			}
 			else{
 				if (control.channel[0] > 0.2){
-					PWM_SetDutyRatio(&pwm2[1],1);
-					PWM_SetDutyRatio(&pwm2[2],0);				
-					PWM_SetDutyRatio(&pwm2[3],0);
-					PWM_SetDutyRatio(&pwm2[4],1);
-					//back
 					PWM_SetDutyRatio(&pwm1[4],1);
 					HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,0);
 					HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,0);
 					HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,1);
+					PWM_SetDutyRatio(&pwm2[1],1);
+					PWM_SetDutyRatio(&pwm2[2],1);
 				}
 				if (control.channel[0] < -0.2){
-					
-					//front
-					PWM_SetDutyRatio(&pwm2[1],0);
-					PWM_SetDutyRatio(&pwm2[2],1);
-					PWM_SetDutyRatio(&pwm2[3],1);
-					PWM_SetDutyRatio(&pwm2[4],0);
-					//back
 					PWM_SetDutyRatio(&pwm1[4],0);
 					HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,1);
 					HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,1);
 					HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,0);
-				}
-				else {
 					PWM_SetDutyRatio(&pwm2[1],1);
 					PWM_SetDutyRatio(&pwm2[2],1);
-					PWM_SetDutyRatio(&pwm2[3],1);
-					PWM_SetDutyRatio(&pwm2[4],1);
-					//back
+				}
+				else {
 					PWM_SetDutyRatio(&pwm1[4],1);
 					HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,1);
 					HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,1);
 					HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,1);
+					PWM_SetDutyRatio(&pwm2[1],1);
+					PWM_SetDutyRatio(&pwm2[2],1);
 				}
 			}
+			*/
+			//!!!!!This is trace module!!!!!! Don't use controller part at a same time!!!!!!!!!!!!! 
+			if (adc_data[0].data > 2050 && adc_data[3].data < 2250 && adc_data[5].data > 2100){
+				PWM_SetDutyRatio(&pwm1[4],1);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,0);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,1);
+				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,0);
+				PWM_SetDutyRatio(&pwm2[1],0.25);
+				PWM_SetDutyRatio(&pwm2[2],0.25);
+			}//on the right way
+			if (adc_data[0].data < 2050 && adc_data[3].data < 2250 && adc_data[5].data > 2100){
+				PWM_SetDutyRatio(&pwm1[4],1);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,0);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,1);
+				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,0);
+				PWM_SetDutyRatio(&pwm2[1],0.25);
+				PWM_SetDutyRatio(&pwm2[2],0.25);
+			}//a little 
+			if (adc_data[0].data > 2050 && adc_data[3].data < 2250 && adc_data[5].data < 2100){
+				PWM_SetDutyRatio(&pwm1[4],1);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,0);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,1);
+				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,0);
+				PWM_SetDutyRatio(&pwm2[1],0.25);
+				PWM_SetDutyRatio(&pwm2[2],0.25);
+			}// a little
+			if (adc_data[0].data < 2050 && adc_data[3].data < 2250 && adc_data[5].data < 2100){
+				PWM_SetDutyRatio(&pwm1[4],1);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,1);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,1);
+				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,1);
+				PWM_SetDutyRatio(&pwm2[1],0.25);
+				PWM_SetDutyRatio(&pwm2[2],0.25);
+			}//parallel black !stop
+			if (adc_data[0].data < 2050 && adc_data[3].data > 2250 && adc_data[5].data > 2100){
+				PWM_SetDutyRatio(&pwm1[4],1);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,1);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,1);
+				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,0);
+				PWM_SetDutyRatio(&pwm2[1],0.25);
+				PWM_SetDutyRatio(&pwm2[2],0.25);
+			}// adjustion
+			if (adc_data[0].data > 2050 && adc_data[3].data > 2250 && adc_data[5].data < 2100){
+				PWM_SetDutyRatio(&pwm1[4],1);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,0);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,1);
+				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,1);
+				PWM_SetDutyRatio(&pwm2[1],0.25);
+				PWM_SetDutyRatio(&pwm2[2],0.25);
+			}// adjustion
+			if (adc_data[0].data > 2050 && adc_data[3].data > 2250 && adc_data[5].data > 2100){
+				PWM_SetDutyRatio(&pwm1[4],1);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,1);
+				HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,1);
+				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,1);
+				PWM_SetDutyRatio(&pwm2[1],0.25);
+				PWM_SetDutyRatio(&pwm2[2],0.25);
+			}// white space !stop
+			
 		}
-		//PWM_SetDutyRatio(&pwm2[1],1);
-		//PWM_SetDutyRatio(&pwm2[2],1);
-		//PWM_SetDutyRatio(&pwm2[3],1);
-		//PWM_SetDutyRatio(&pwm2[4],1);
 		/* USER CODE END */
 		delay(5);
 }
